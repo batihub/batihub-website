614131782b3e992f1ecfee095ded69df7c7e4d8c
batıhan babacan <reichnein1234bathan@gmail.com>
2025-12-04 03:10:26 +0300

    Enhance CRUD operations for posts and authors
    Updated create_post_postgres to include author_id and added create_author_postgres function. Implemented get_authors_all_posts and get_all_authors functions for fetching posts and authors.

--- Backend/crud_postgres.py
+++ Backend/crud_postgres.py
@@ -1,12 +1,13 @@
 import logging
-from typing import Union, Optional, List
+from typing import Optional, Sequence
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, Boolean
-from postgreq_database import PostPostgres
+from sqlalchemy import select
+from postgreq_database import PostPostgres, AuthorPostgres
 
-async def create_post_postgres(db:AsyncSession, title:str, content:str, is_published:bool=True) -> Optional[PostPostgres]:
+
+async def create_post_postgres(db:AsyncSession, title:str, content:str, author_id : int, is_published:bool=True) -> Optional[PostPostgres]:
     try:
-        new_post = PostPostgres(title=title, content=content, is_published=is_published)
+        new_post = PostPostgres(title=title, content=content, author_id= author_id , is_published=is_published)
         db.add(new_post)
         await db.commit()
         await db.refresh(new_post)  ####### object is stale(missing id and timestamps) Necessary: To retrieve the database-generated primary key (id) and timestamps before returning the object
@@ -15,7 +16,29 @@ async def create_post_postgres(db:AsyncSession, title:str, content:str, is_publi
         await db.rollback()
         logging.error(f"error when adding to database {e}")
 
-async def get_all_posts_postgres(db:AsyncSession) -> Optional[List[PostPostgres]]:
+
+async def create_author_postgres(db:AsyncSession,name: str) -> Optional[AuthorPostgres]:
+    try:
+        new_author = AuthorPostgres(name = name)
+        db.add(new_author)
+        await db.commit()
+        await db.refresh(new_author)  ####### object is stale(missing id and timestamps) Necessary: To retrieve the database-generated primary key (id) and timestamps before returning the object
+        return new_author
+    except Exception as e:
+        await db.rollback()
+        logging.error(f"error when adding to database {e}")
+
+async def get_authors_all_posts(db:AsyncSession, author_id: int):
+    try:
+        q = select(PostPostgres).where(PostPostgres.author_id == author_id)
+        result = await db.execute(q)
+        return result.scalars().all()
+    except Exception as e:
+        logging.error(f"error when fetching authors all posts : {e}")
+        return None
+
+
+async def get_all_posts_postgres(db:AsyncSession) -> Sequence[PostPostgres] | None:
     try:
         q = select(PostPostgres).order_by(PostPostgres.id)    #############################
         result = await db.execute(q)
@@ -26,6 +49,15 @@ async def get_all_posts_postgres(db:AsyncSession) -> Optional[List[PostPostgres]
     except Exception as e:
         logging.error(f"error when fetching all posts : {e}")
         return None
+async def get_all_authors(db: AsyncSession):
+    try:
+        q = select(AuthorPostgres).order_by(AuthorPostgres.id)
+        result = await db.execute(q)
+        return result.scalars().all()
+    except Exception as e:
+        logging.error(f"error when fetching all authors : {e}")
+        return None
+
 
 async def delete_post_by_id_postgres(db:AsyncSession, post_id: int) -> bool:
     try:
